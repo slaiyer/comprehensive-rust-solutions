8b9f5e4d89420cde79e05c6f9e8efd848eed0ad3
slaiyer <1776970+slaiyer@users.noreply.github.com>
2023-10-31 16:59:21 +0530

    handle warnings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
-}
+#![allow(unused_variables, dead_code)]
+
+#[path="./day-1-pattern-matching.rs"]
+mod pattern_matching;
+
+#[path="./day-2-afternoon-string-iterators.rs"]
+mod string_iterators;
+
+fn main() {}
